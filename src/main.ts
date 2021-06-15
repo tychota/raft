6008a3443800b7785189b5f5e8735f1504d5d472
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-06-15 12:12:14 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { container } from "tsyringe";
 import { ConsensusController } from "@consensus/infrastructure/consensus.controller";
 const controller = container.resolve(ConsensusController);
 
-import { Server } from "modules/api/infrastructure/server";
-const server = new Server();
-server.listen();
+import { Server } from "@api/infrastructure/server";
+const server = container.resolve(Server)
+server.listen()
--- src/modules/api/infrastructure/server.ts
+++ src/modules/api/infrastructure/server.ts
@@ -1,25 +1,50 @@
+import { injectable } from "tsyringe";
+
 import { createServer, Socket, Server as NetServer } from "net";
 
+@injectable()
 export class Server {
   private readonly net: NetServer;
+
   constructor() {
     this.net = createServer();
     this.net.on("close", this.onClose);
     this.net.on("connection", this.onConnection);
   }
 
-  onClose = () => {
+  private onClose = () => {
     console.log("Server closed !");
   };
 
-  onConnection = (socket: Socket) => {
+  private onConnection = (socket: Socket) => {
+    this.logConnection(socket);
+    socket.setEncoding("utf8");
+    socket.on("data", (data) => this.onData(data, socket));
+  };
+
+  private onData = (data: Buffer, socket: Socket) => {
+    var bread = socket.bytesRead;
+    var bwrite = socket.bytesWritten;
+    console.log("Bytes read : " + bread);
+    console.log("Bytes written : " + bwrite);
+    console.log("Data sent to server : " + data);
+
+    //echo data
+    var is_kernel_buffer_full = socket.write("Data ::" + data);
+    if (is_kernel_buffer_full) {
+      console.log("Data was flushed successfully from kernel buffer i.e written successfully!");
+    } else {
+      socket.pause();
+    }
+  };
+
+  private logConnection = (socket: Socket) => {
     //this property shows the number of characters currently buffered to be written. (Number of characters is approximately equal to the number of bytes to be written, but the buffer may contain strings, and the strings are lazily encoded, so the exact number of bytes is not known.)
     //Users who experience large or growing bufferSize should attempt to "throttle" the data flows in their program with pause() and resume().
 
     console.log("Buffer size : " + socket.writableLength);
 
     console.log("---------server details -------------------");
-
     const address = this.net.address();
     if (address && typeof address !== "string") {
       console.log(`Server is listening at port ${address.port}`);
@@ -30,18 +55,10 @@ export class Server {
     console.log(`Server LOCAL ip is  ${socket.localAddress}`);
 
     console.log("------------remote client info ------------");
-
-    var rport = socket.remotePort;
-    var raddr = socket.remoteAddress;
-    var rfamily = socket.remoteFamily;
-
     console.log(`REMOTE Socket is listening at port ${socket.remotePort}`);
     console.log(`REMOTE Socket ip is ${socket.remoteAddress}`);
     console.log(`REMOTE Socket is family (IP4/IP6) is ${socket.remoteFamily}`);
-
     console.log("-------------------------------------------");
-
-    socket.on("data", console.log);
   };
 
   listen() {
